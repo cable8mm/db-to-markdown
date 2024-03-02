c6cd04c91ccf6b87158b206c30a64a1d9f1be1c2
Sam Lee <cable8mm@gmail.com>
2024-03-03 03:21:17 +1300

    Create the command to convert jekyll files

---
+++ src/Command/CreateJekyllCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Cable8mm\DbToMarkdown\Command;
+
+use Cable8mm\DbToMarkdown\DB;
+use Cable8mm\DbToMarkdown\Formats\Jekyll;
+use Cable8mm\DbToMarkdown\Mappers\Mapper;
+use Cable8mm\DbToMarkdown\Models\Article;
+use Medoo\Medoo;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'create-jekyll',
+    description: 'Creates markdowns. run bin/console create-jekyll',
+    hidden: false,
+    aliases: ['add-jekyll']
+)]
+class CreateJekyllCommand extends Command
+{
+    private Medoo $database;
+
+    protected function configure()
+    {
+        $this->database = DB::getInstance()->getConnection();
+    }
+
+    /**
+     * Create Markdown files for Jekyll
+     *
+     * Run bin/console create-jekyll
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $map = Mapper::getMap('DogStory');
+
+        $mapper = new Mapper(...$map);
+
+        $articles = $this->database->select(DB::table(), $mapper->fields(), $mapper->where());
+
+        foreach ($articles as $row) {
+            $article = Article::make($map)->in($row);
+
+            $jekyll = new Jekyll(
+                layout: 'single',
+                title: $article->title,
+                date: $article->publishedAt,
+                author: 'Samgu Lee',
+                body: $article->markdown(),
+                slug: $article->slug
+            );
+
+            file_put_contents(__DIR__.'/../../dist/'.$jekyll->path(), $jekyll->render());
+        }
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Formats/Jekyll.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Cable8mm\DbToMarkdown\Formats;
+
+use Carbon\Carbon;
+
+class Jekyll implements IFormats
+{
+    public function __construct(
+        private string $layout,
+        private string $title,
+        private Carbon $date,
+        private string $author,
+        private string $body,
+        private string $slug,
+        private ?string $categories = null
+    ) {
+
+    }
+
+    public function path(string $extention = 'markdown'): string
+    {
+        $prefix = $this->date->format('Y-m-d');
+
+        $filename = preg_replace('/[_ ]/', '-', $this->slug);
+
+        return $prefix.'-'.$filename.'.'.$extention;
+    }
+
+    public function render(): string
+    {
+        $out = '---'.PHP_EOL;
+        $out .= 'layout : '.$this->layout.PHP_EOL;
+        $out .= 'title : "'.$this->title.'"'.PHP_EOL;
+        $out .= 'date : '.$this->date->format('Y-m-d h:i:s').PHP_EOL;
+
+        if (! is_null($this->categories)) {
+            $out .= 'categories : '.$this->categories.PHP_EOL;
+        }
+
+        $out .= 'author : '.$this->author.PHP_EOL;
+        $out .= '---'.PHP_EOL;
+
+        $out .= $this->body;
+
+        return $out;
+    }
+}
