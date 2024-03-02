126cc08da9f4e41d2223db92c25f2a44ec828678
Samgu Lee <cable8mm@gmail.com>
2024-03-03 00:31:41 +1300

    Generate from db to markdown files (#3)

--- src/Command/CreateMarkdownsCommand.php
+++ src/Command/CreateMarkdownsCommand.php
@@ -4,6 +4,7 @@ namespace Cable8mm\ImportFromDbToJekyll\Command;
 
 use Cable8mm\ImportFromDbToJekyll\DB;
 use Cable8mm\ImportFromDbToJekyll\Mappers\Mapper;
+use Cable8mm\ImportFromDbToJekyll\Models\Article;
 use Medoo\Medoo;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -25,13 +26,24 @@ class CreateMarkdownsCommand extends Command
         $this->database = DB::getInstance()->getConnection();
     }
 
+    /**
+     * Create Markdown files
+     *
+     * Run ./bin/console create-markdowns
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $map = Mapper::getMap('DogStory');
 
         $mapper = new Mapper(...$map);
 
-        $articles = $this->database->select(DB::table(), $mapper->fields());
+        $articles = $this->database->select(DB::table(), $mapper->fields(), $mapper->where());
+
+        foreach ($articles as $row) {
+            $article = Article::make($map)->in($row);
+
+            file_put_contents(__DIR__.'/../../dist/'.$article->permalink, $article->markdown());
+        }
 
         return Command::SUCCESS;
     }
