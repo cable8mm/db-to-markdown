96b67582143f2395a00a84e63e23d783f67a3336
Samgu Lee <cable8mm@gmail.com>
2024-03-05 14:18:30 +1300

    Implement a seeding command upon package installation (#10)
    * Implement a seeding command upon package installation
    
    * Address database and testing bugs

--- src/Command/SeedingCommand.php
+++ src/Command/SeedingCommand.php
@@ -2,28 +2,33 @@
 
 namespace Cable8mm\DbToMarkdown\Command;
 
+use Cable8mm\Database\Model;
+use Cable8mm\Database\Seeder;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
-    name: 'clean',
-    description: 'Delete markdowns. run bin/console clean',
+    name: 'seeding',
+    description: 'Seeding. run bin/console seeding',
     hidden: false,
-    aliases: ['trash', 'empty']
+    aliases: ['seed']
 )]
-class CleanCommand extends Command
+class SeedingCommand extends Command
 {
     /**
-     * Create Markdown files
+     * Seeding
      *
-     * Run bin/console delete-markdowns
+     * Run bin/console seeding
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        array_map('unlink', array_filter((array) glob(__DIR__.'/../../dist/*.markdown')));
-        array_map('unlink', array_filter((array) glob(__DIR__.'/../../dist/*.md')));
+        Model::getInstance()->down();
+
+        Model::getInstance()->up();
+
+        (new Seeder())->run();
 
         return Command::SUCCESS;
     }
